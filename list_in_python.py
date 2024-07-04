bbeb1a72c5fa4e30f13773b992e758fc81182d4d
mahabube <info.freelancermahabub@gmail.com>
2024-07-04 20:29:07 +0600

    added

--- list_in_python.py
+++ list_in_python.py
@@ -1,2 +1,16 @@
 countries =  ['United Kingdom', 'Bangladesh', 'Ghana', 'Nigerian', 'Albanian']
 print(countries[0])
+print(countries[1:4])
+print(type(countries))
+name = 'Mahbub'
+print(type(name))
+countries[0] = 'United States'
+print(countries)
+print(countries[-1])
+print(len(countries))
+lists = ['United States', 2, True, 'Australia']
+print(type(lists))
+print(type(lists[2]))
+
+lists2 = list(('Nigerian', 34, False))
+print(lists2)
